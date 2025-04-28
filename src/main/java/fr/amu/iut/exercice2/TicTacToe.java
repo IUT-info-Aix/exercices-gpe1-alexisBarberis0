bb90c79b53fde8b1f071691db1d63c57ac09d989
Barberis Alexis <alexis.barberis@etu.univ-amu.fr>
2025-04-29 00:34:00 +0200

    Exercice 2 terminé

--- src/main/java/fr/amu/iut/exercice2/TicTacToe.java
+++ src/main/java/fr/amu/iut/exercice2/TicTacToe.java
@@ -4,59 +4,33 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import java.util.Random;
 
 public class TicTacToe extends Application {
 
     @Override
     public void start(Stage primaryStage) {
         GridPane rootGrid = new GridPane();
-        ImageView caseVide = new ImageView("exercice2/Vide.png");
-        ImageView caseCroix = new ImageView("exercice2/Croix.png");
-        ImageView caseRond= new ImageView("exercice2/Rond.png");
+        String[] images = {"Vide.png","Croix.png","Rond.png"};
+        Random rand = new Random();
 
-        Label case0_0 = new Label();
-        case0_0.setGraphic(caseVide);
-        rootGrid.add(case0_0, 0, 0);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
 
-        Label case0_1 = new Label();
-        case0_1.setGraphic(caseVide);
-        rootGrid.add(case0_1, 0, 1);
+                Label caseLabel = new Label();
+                caseLabel.setGraphic(new ImageView("/exercice2/" + images[rand.nextInt(3)]));
+                rootGrid.add(caseLabel, i, j);
 
-        Label case0_2 = new Label();
-        case0_2.setGraphic(caseVide);
-        rootGrid.add(case0_2, 0,2);
-
-        Label case1_0 = new Label();
-        case1_0.setGraphic(caseVide);
-        rootGrid.add(case1_0, 1,0);
-
-        Label case1_1 = new Label();
-        case1_1.setGraphic(caseVide);
-        rootGrid.add(case1_1, 1,1);
-
-        Label case1_2 = new Label();
-        case1_2.setGraphic(caseVide);
-        rootGrid.add(case1_2, 1,2);
-
-        Label case2_0 = new Label();
-        case2_0.setGraphic(caseVide);
-        rootGrid.add(case2_0, 2,0);
-
-        Label case2_1 = new Label();
-        case2_1.setGraphic(caseVide);
-        rootGrid.add(case2_1, 2,1);
-
-        Label case2_2 = new Label();
-        case2_2.setGraphic(caseVide);
-        rootGrid.add(case2_2, 2,2);
+            }
+        }
+        rootGrid.setGridLinesVisible(true);
 
         Scene scene = new Scene(rootGrid);
         primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Tic Tac Toe");
         primaryStage.show();
     }
 }
